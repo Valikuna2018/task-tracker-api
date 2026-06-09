e6142a61b21937a124475154b806353d6c22ef06
Valeriane <valerianemur@gmail.com>
2026-06-10 03:00:22 +0400

    add repositories

---
+++ src/main/java/com/example/tasktracker/repository/ProjectRepository.java
@@ -0,0 +1,14 @@
+package com.example.tasktracker.repository;
+
+import com.example.tasktracker.model.Project;
+import com.example.tasktracker.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProjectRepository extends JpaRepository<Project,Long> {
+    List<Project> findByOwner(User owner);
+
+
+}
