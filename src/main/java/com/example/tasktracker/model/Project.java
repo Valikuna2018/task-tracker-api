fc0625457b283219bb2d8b0700935007c7b7f2d7
Valeriane <valerianemur@gmail.com>
2026-06-10 02:13:29 +0400

    add model of project

---
+++ src/main/java/com/example/tasktracker/model/Project.java
@@ -0,0 +1,40 @@
+package com.example.tasktracker.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "projects")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 1000)
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    @CreationTimestamp
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createDate;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updateDate;
+}
