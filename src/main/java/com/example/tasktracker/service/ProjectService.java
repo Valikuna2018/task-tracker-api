d592d55ce7d8c9c684bd6abf8052cc2ed66dd510
Valeriane <valerianemur@gmail.com>
2026-06-11 02:51:38 +0400

    add project controler,service

---
+++ src/main/java/com/example/tasktracker/service/ProjectService.java
@@ -0,0 +1,75 @@
+package com.example.tasktracker.service;
+
+import com.example.tasktracker.dto.project.ProjectRequest;
+import com.example.tasktracker.dto.project.ProjectResponse;
+import com.example.tasktracker.mapper.ProjectMapper;
+import com.example.tasktracker.model.Project;
+import com.example.tasktracker.model.User;
+import com.example.tasktracker.repository.ProjectRepository;
+import com.example.tasktracker.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProjectService {
+
+    private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
+    private final ProjectMapper projectMapper;
+
+    public ProjectResponse createProject(ProjectRequest request) {
+
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+
+        String email = authentication.getName();
+
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("user not found"));
+
+        Project project = projectMapper.toEntity(request);
+
+        project.setOwner(user);
+        Project saveProject = projectRepository.save(project);
+
+        return projectMapper.toResponse(saveProject);
+    }
+
+    public List<ProjectResponse> getAllProjects() {
+        return projectRepository.findAll()
+                .stream()
+                .map(p -> projectMapper.toResponse(p))
+                .toList();
+    }
+
+    public ProjectResponse getProjectById(Long id) {
+        Project project = projectRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Project not found"));
+
+        return projectMapper.toResponse(project);
+    }
+
+    public ProjectResponse updateProject(Long id, ProjectRequest request) {
+        Project project = projectRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Project not found"));
+
+        project.setName(request.getName());
+        project.setDescription(request.getDescription());
+
+        Project updatedProject = projectRepository.save(project);
+
+        return projectMapper.toResponse(updatedProject);
+    }
+
+    public void deleteProject(Long id) {
+        if (!projectRepository.existsById(id)) {
+            throw new RuntimeException("Project not found");
+        }
+
+        projectRepository.deleteById(id);
+    }
+}
